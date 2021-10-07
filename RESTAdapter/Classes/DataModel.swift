65c02846a4b74505e3dcd11e4c6c414c9bf5c63f
Skibin Alexander <skibinalexander@gmail.com>
2021-10-07 14:56:34 +0300

    fix update

--- RESTAdapter/Classes/DataModel.swift
+++ RESTAdapter/Classes/DataModel.swift
@@ -13,10 +13,10 @@ public protocol DataModel {
     
     /// Инициализатор
     /// - Parameter json: Data JSON object
-    init(json: JSON)
+    public init(json: JSON)
     
     /// Опциональный инициализатор
     /// - Parameter json: Data JSON object optional
-    init?(json: JSON?)
+    public init?(json: JSON?)
     
 }
